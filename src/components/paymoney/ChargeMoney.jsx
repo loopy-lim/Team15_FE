190c09d8ea8207a301cebe76714d860ab7574a9a
minkonging <skysmksky@gmail.com>
2023-10-05 14:14:48 +0900

    feat: 충전하기 버튼 추가

---
+++ src/components/paymoney/ChargeMoney.jsx
@@ -0,0 +1,11 @@
+import { Txt } from "../common/Txt.component"
+
+export const ChargeMoney = () => {
+    return (
+        <>
+            <div className="bg-[#F1F1F1] p-2 m-6 rounded-lg w-[25rem] h-[5rem] flex ">
+                <p className="float-left mr-4 text-gray-500">충전금액</p>
+            </div>
+        </>
+    )
+}
--- src/pages/Charge.page.jsx
+++ src/pages/Charge.page.jsx
@@ -1,6 +1,9 @@
 import { isIOS, isMobile } from "react-device-detect";
 import { AppBar } from "../components/common/AppBar.component";
 import { BASE_PADDING } from "../constants";
+import classnames from "classnames";
+import { ChargeMoney } from "../components/paymoney/ChargeMoney";
+import { BottomFullLink } from "../components/common/BottomFullLink.component";
 
 export const ChargePage = () => {
 
@@ -17,6 +20,8 @@ export const ChargePage = () => {
                         : "h-[calc(100vh-8.5rem)]"
                 )}
             >
+                <ChargeMoney />
+                <BottomFullLink title="충전하기" />
 
             </main>
         </>
