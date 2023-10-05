99d1561a78bd1b13a4944ec9db5838289d0609b3
minkonging <skysmksky@gmail.com>
2023-10-05 13:51:04 +0900

    feat: ChargePage 생성

--- src/pages/Charge.page.jsx
+++ src/pages/Charge.page.jsx
@@ -1,4 +1,24 @@
+import { isIOS, isMobile } from "react-device-detect";
+import { AppBar } from "../components/common/AppBar.component";
+import { BASE_PADDING } from "../constants";
 
 export const ChargePage = () => {
 
+    return (
+        <>
+            <AppBar to="/paymoney" title={"충전"} br={true} />
+            <main
+                aria-label="main"
+                className={classnames(
+                    "bg-white overflow-auto",
+                    BASE_PADDING,
+                    isMobile && isIOS
+                        ? "h-[calc(100vh-10rem-env(safe-area-inset-top))]"
+                        : "h-[calc(100vh-8.5rem)]"
+                )}
+            >
+
+            </main>
+        </>
+    )
 };
