c3f08a2ae8fe8baf2437e14af160c884d1a366d9
loopy-lim <loopy@jnu.ac.kr>
2023-10-14 11:31:18 +0900

    feat: add user mocks

---
+++ src/mocks/handlers/index.js
@@ -0,0 +1,3 @@
+import { userMocks } from "./user.mocks";
+
+export const handler = [...userMocks];
--- src/mocks/handlers/user.mocks.js
+++ src/mocks/handlers/user.mocks.js
@@ -1,9 +1,64 @@
 import { rest } from "msw";
 
-export const UserMocks = [
+export const userMocks = [
   rest.post("/user/join", (req, res, ctx) => {
     return res(
       ctx.status(201),
+      ctx.json({
+        success: true,
+        response: null,
+        error: null,
+      })
+    );
+  }),
+  rest.post("/user/join/check", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: true,
+        response: null,
+        error: null,
+      })
+    );
+  }),
+  rest.post("/user/login", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: true,
+        response: null,
+        error: null,
+      })
+    );
+  }),
+  rest.post("/user/logout", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: true,
+        response: null,
+        error: null,
+      })
+    );
+  }),
+  rest.get("/user", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: true,
+        response: {
+          id: 1,
+          universityName: "전남대학교",
+          nickname: "loopy",
+          idCardImagePath: "https://placehold.co/400",
+        },
+        error: null,
+      })
+    );
+  }),
+  rest.patch("/user", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
       ctx.json({
         success: true,
         response: null,
