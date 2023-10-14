886d344daa4ef4f01f422effd108faa2f5787854
loopy-lim <loopy@jnu.ac.kr>
2023-10-15 01:32:42 +0900

    feat: add rental mocks

--- src/mocks/handlers/index.js
+++ src/mocks/handlers/index.js
@@ -2,10 +2,12 @@ import { userMocks } from "./user.mocks";
 import { productMocks } from "./product.mocks";
 import { paymentMocks } from "./payment.mocks";
 import { categoryMocks } from "./category.mocks";
+import { rentalMocks } from "./rental.mocks";
 
 export const handler = [
   ...userMocks,
   ...productMocks,
   ...paymentMocks,
   ...categoryMocks,
+  ...rentalMocks,
 ];
---
+++ src/mocks/handlers/rental.mocks.js
@@ -0,0 +1,60 @@
+import { rest } from "msw";
+
+export const rentalMocks = [
+  rest.get("/rental", (req, res, ctx) => {
+    return {
+      success: true,
+      response: [
+        {
+          productId: 1,
+          name: "아이패드",
+          borrowAt: `${new Date(
+            new Date().getTime() + 10 * 60 * 60 * 24 * 1000
+          )}`,
+          returnAt: `${new Date(
+            new Date().getTime() + 30 * 60 * 60 * 24 * 1000
+          )}`,
+          reviewed: false,
+        },
+        {
+          productId: 2,
+          name: "아이패드",
+          borrowAt: `${new Date()}`,
+          returnAt: `${new Date(
+            new Date().getTime() + 30 * 60 * 60 * 24 * 1000
+          )}`,
+          reviewed: false,
+        },
+        {
+          productId: 3,
+          name: "아이패드",
+          borrowAt: `${new Date(
+            new Date().getTime() - 30 * 60 * 60 * 24 * 1000
+          )}`,
+          returnAt: `${new Date(new Date().getTime() - 30 * 60 * 60 * 1000)}`,
+          reviewed: false,
+        },
+        {
+          productId: 4,
+          name: "아이패드",
+          borrowAt: `${new Date(
+            new Date().getTime() - 30 * 60 * 60 * 24 * 1000
+          )}`,
+          returnAt: `${new Date(new Date().getTime() - 30 * 60 * 60 * 1000)}`,
+          reviewed: true,
+        },
+      ],
+      error: null,
+    };
+  }),
+  rest.post("/rental/:id/return", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        success: true,
+        response: null,
+        error: null,
+      })
+    );
+  }),
+];
