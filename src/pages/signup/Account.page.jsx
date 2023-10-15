fc876ce739354826a0ae364ea87bc22acc9a9ec1
mlnwns <mlnwns@naver.com>
2023-10-15 20:07:26 +0900

    feat: add check information

--- src/pages/signup/Account.page.jsx
+++ src/pages/signup/Account.page.jsx
@@ -1,6 +1,3 @@
-import { BASE_PADDING } from "../../constants";
-import { isIOS, isMobile } from "react-device-detect";
-import classnames from "classnames";
 import { BottomFullLink } from "../../components/common/BottomFullLink.component";
 import {
   InputPassword,
@@ -9,8 +6,37 @@ import {
 import { SlidePannels } from "../../components/sign/SlidePannel.component";
 import { SignInformation } from "../../components/sign/Information.component";
 import { MainContainer } from "../../components/common/MainContainer.component";
+import { useState } from "react";
+import { Txt } from "../../components/common/Txt.component";
 
 export const AccountPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [canNext, setCanNext] = useState(false);
+
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const passwordPattern =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    updateCanNext(newEmail, password);
+  };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    updateCanNext(email, newPassword);
+  };
+
+  const updateCanNext = (newEmail, newPassword) => {
+    const isEmailValid = emailPattern.test(newEmail);
+    const isPasswordValid = passwordPattern.test(newPassword);
+    setCanNext(isEmailValid && isPasswordValid);
+  };
+
   return (
     <MainContainer>
       <div className="mt-20">
@@ -18,12 +44,23 @@ export const AccountPage = () => {
       </div>
       <div className="flex flex-col items-center">
         <SignInformation title={`이메일과 비밀번호를\n설정하세요`} />
-        <div className="flex flex-col gap-4 w-3/4">
-          <InputText label="Email" />
-          <InputPassword label="Password" />
+        <div class="flex flex-col gap-4 w-3/4">
+          <InputText label="Email" value={email} onChange={handleEmailChange} />
+          <InputPassword
+            label="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Txt typography="subtitle" colors="secondaryLight">
+            비밀번호는 영어와 특수문자 포함하는 8자 이상
+          </Txt>
         </div>
       </div>
-      <BottomFullLink title="다음으로" to={`/signup/school`} />
+      <BottomFullLink
+        title="다음으로"
+        to={`/signup/school`}
+        isActive={canNext}
+      />
     </MainContainer>
   );
 };
