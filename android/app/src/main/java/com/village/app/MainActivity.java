ef5c9aae9ac404701c7210c1cd7944ba3a939e21
loopy-lim <loopy@jnu.ac.kr>
2023-11-03 00:03:03 +0900

    :feat: init backend branch

--- android/app/src/main/java/com/village/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.village.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {},
-});
